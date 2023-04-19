699ac2b3a49938d5467bc4d7032442d9b06a0f77
CJ3016846 <CJ3016846@LAB01-PC17>
2023-04-19 12:50:53 -0300

    Adicionar arquivos de projeto.

---
+++ prova1bi/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prova1bi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int ex;
+            //Console.WriteLine("escolha um exercício");
+            //Console.WriteLine("exercicio 1");
+            //Console.WriteLine("exercicio 2");
+            //Console.WriteLine("exercicio 3");
+            //Console.WriteLine("exercicio 4");
+            //Console.WriteLine("exercicio 5");
+            //Console.WriteLine("exercicio 6");
+            //Console.WriteLine("exercicio 7");
+            //ex = int.Parse(Console.ReadLine());
+            //ex1
+            //switch(ex)
+            //{
+            //case 1: 
+
+            // double num;
+            // Console.WriteLine(" digite um numero");
+            //num = double.Parse(Console.ReadLine());
+
+            //if (num < 0 * -1) ;
+            //{
+            //Console.WriteLine(" este é o resultado");
+            //else if (num > 0 ) ;
+            //}
+            //Console.WriteLine("o número digitado continua o mesmo");
+            //break;
+
+            //ex2
+
+            //switch (ex)
+            // {
+            //case 1:
+            //int número;
+            // Console.WriteLine("digite algum número para realizar o programa");
+            //número = int.Parse(Console.ReadLine());
+
+            //if(número / 3 == 0 && número / 5 == 0);
+            //{
+            //Console.WriteLine("este número é divisivel por 3 e 5");
+            /// }
+            //else if (número / 4 == 0 && número / 7 == 0) ;
+            // {
+            //   Console.WriteLine(" este número é divisivel por 4 e 7");
+            // }
+            // else if  (número !=) ;
+            //   { 
+            //Console.WriteLine("não é possível realizar a divisão com esse número");
+            // break;
+
+            //ex4
+
+            //Console.WriteLine("escolha um dia");
+            // Console.WriteLine("domingo");
+            //  Console.WriteLine("segunda");
+            //   Console.WriteLine("terça");
+            //    Console.WriteLine("quarta");
+            //     Console.WriteLine("quinta");
+            //      Console.WriteLine("sexta");
+            //       Console.WriteLine("sexta");
+            //switch (dia)
+            //{
+            //    case 1:
+            //        int dia;
+            //        Console.WriteLine("domingo jogo futebol");
+            //        break;
+            //    case 2:
+            //        int dia;
+            //        Console.WriteLine("segunda assisto aula de filisofia, portugues e matematica");
+            //        break;
+            //    case 3:
+            //        int dia;
+            //            Console.WriteLine("terça assisto quimica, historia, imi");
+            //        break;
+            //    case 4:
+            //        int dia;
+            //        Console.WriteLine("qurta assisto biologia, portugues, programação");
+            //        break;
+            //        case 5;
+            //        int dia;
+            //        Console.WriteLine("quinta assisto fisica, sociologia, matematica");
+            //        break;
+            //    case 6:
+            //        int dia;
+            //        Console.WriteLine(" sexta assisto geografia, portugues, bds");
+            //        break;
+
+            //        else if (!=dia);
+            //        Console.WriteLine(" este dia é invalido");
+            //        Console.ReadLine());
+            //ex5
+            //int num;
+            //Console.WriteLine("escolha um numero ");
+            // Console.WriteLine("1");
+            //Console.WriteLine("2");
+            // Console.WriteLine("3");
+            //  Console.WriteLine("4");
+            //    Console.WriteLine("5");
+            //     Console.WriteLine("6");
+            //      Console.WriteLine("7");
+            //Console.WriteLine("8");
+            //Console.WriteLine("9");
+
+            //if (num > 8) ;
+            //Console.WriteLine(" este é o maior numero da sequncia");
+            //else if (num > 2) ;
+            //Console.WriteLine("este é o menor numero da sequencia");
+            //else if (num = 0) ;
+            //Console.WriteLine(" este numero se mantem");
+            //Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
